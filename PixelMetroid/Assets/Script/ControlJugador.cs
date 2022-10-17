bf2cb28c30c43981e2854b013bbe12c71b7a1222
Mgarciac105 <manuelgarciacasas8@gmail.com>
2022-10-17 18:51:48 +0200

    Version pruebas gitignore 0.3

---
+++ PixelMetroid/Assets/Script/ControlJugador.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlJugador : MonoBehaviour
+{
+
+    public int velocidad;
+    private Rigidbody2D fisica;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fisica = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float entradaX = Input.GetAxis("Horizontal");
+        //fisica.velocity = new Vector2(entradaX * velocidad , fisica.velocity.y);ç
+
+        if( (Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.LeftArrow)))
+        {
+            transform.Translate(new Vector2(-0.05f, 0.0f));
+        }
+    }
+
+}
