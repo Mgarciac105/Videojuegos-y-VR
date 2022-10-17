829e5fd52cceeed38526d18cbd0ae3314238957a
Mgarciac105 <manuelgarciacasas8@gmail.com>
2022-10-17 18:11:21 +0200

    Version prueba gitignore 0.1

---
+++ MiPrimerProyecto/Assets/Scripts/ScriptCubo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptCubo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mensaje de informacion");
+        Debug.LogWarning("Mensaje de advertencia");
+        Debug.LogError("Mensaje de error");
+    }
+
+}
