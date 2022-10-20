37a41274880f4bb00b0906b2dba0fe1f76e583b5
Mgarciac105 <manuelgarciacasas8@gmail.com>
2022-10-20 18:01:38 +0200

    Version 20/10 Beta

--- PixelMetroid/Assets/Script/ControlJugador.cs
+++ PixelMetroid/Assets/Script/ControlJugador.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class ControlJugador : MonoBehaviour
 {
-
     public int velocidad;
     private Rigidbody2D fisica;
 
@@ -17,13 +16,13 @@ public class ControlJugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //float entradaX = Input.GetAxis("Horizontal");
-        //fisica.velocity = new Vector2(entradaX * velocidad , fisica.velocity.y);ç
+        float entradaX = Input.GetAxis("Horizontal");
+        fisica.velocity = new Vector2(entradaX * velocidad, fisica.velocity.y);
 
-        if( (Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.LeftArrow)))
-        {
-            transform.Translate(new Vector2(-0.05f, 0.0f));
-        }
+        //if( (Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.LeftArrow)))
+        //{
+        //    transform.Translate(new Vector2(-0.05f, 0.0f));
+        //}
     }
 
 }
