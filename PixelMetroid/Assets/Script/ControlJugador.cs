77aaedbf9c17904117ca10c405e92b10be710c3d
Mgarciac105 <manuelgarciacasas8@gmail.com>
2022-10-20 20:42:15 +0200

    Version 20/10

--- PixelMetroid/Assets/Script/ControlJugador.cs
+++ PixelMetroid/Assets/Script/ControlJugador.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class ControlJugador : MonoBehaviour
 {
     public int velocidad;
+    public int fuerzaSalto;
     private Rigidbody2D fisica;
-
+    private SpriteRenderer sprite;
+    private float alturaCentro;
     // Start is called before the first frame update
     void Start()
     {
+
         fisica = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        alturaCentro = GetComponent<CapsuleCollider2D>().size.y / 2 - GetComponent<CapsuleCollider2D>().offset.y;
     }
 
     // Update is called once per frame
@@ -19,10 +24,33 @@ public class ControlJugador : MonoBehaviour
         float entradaX = Input.GetAxis("Horizontal");
         fisica.velocity = new Vector2(entradaX * velocidad, fisica.velocity.y);
 
+        if (entradaX < 0) sprite.flipX = true;
+        if (entradaX > 0) sprite.flipX = false;
+
+        //float entradaSalto = Input.GetAxis("Jump");
+        //fisica.AddForce(Vector2.up * (entradaSalto * fuerzaSalto), ForceMode2D.Impulse);
+
+        if (TocarSuelo() && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)))
+        {
+            fisica.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+        }
+
         //if( (Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.LeftArrow)))
         //{
         //    transform.Translate(new Vector2(-0.05f, 0.0f));
         //}
     }
 
+
+    private bool TocarSuelo()
+    {
+        
+        RaycastHit2D tocando = Physics2D.Raycast(transform.position + new Vector3(0,-alturaCentro,0), Vector2.down, 0.56f);
+        //Debug.DrawRay(transform.position, Vector2.down, Color.red,6f);
+        return (tocando.collider != null);
+          
+    }
+
 }
+
+
