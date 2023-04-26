96f6b5bf97c456c1d2c069431d6cbd461760da3f
Mgarciac105 <manuelgarciacasas8@gmail.com>
2023-04-26 18:45:26 +0200

    version 26/7/2023 V2

--- ProtoMinecraft/Assets/Script/GeneracionMapa.cs
+++ ProtoMinecraft/Assets/Script/GeneracionMapa.cs
@@ -7,14 +7,14 @@ public class GeneracionMapa : MonoBehaviour
 
     public int width, height, depth;
 
-    public GameObject tierra, agua, montaña;
+    public GameObject tierra, agua, montaña,tierraArbol,tierraCesped;
 
     public int seed;
     public float detail;
 
     private int[,] perlinNoiseArray;
 
-    private int min, max;
+    private int min, max, rand;
 
     void Start()
     {
@@ -36,19 +36,31 @@ public class GeneracionMapa : MonoBehaviour
     
                 for (int y = 0; y < perlinNoiseArray[x,z]; y++)
                 {
-                    
-                    if(perlinNoiseArray[x,z] < min + 1)
+
+
+                    if (perlinNoiseArray[x, z] < min + 1)
                     {
                         Instantiate(agua, new Vector3(x, y, z), Quaternion.identity);
 
                     }
-                    else if(perlinNoiseArray[x,z] > max - 5)
+                    else if (perlinNoiseArray[x, z] > max - 5)
                     {
                         Instantiate(montaña, new Vector3(x, y, z), Quaternion.identity);
 
                     }
-                    else Instantiate(tierra, new Vector3(x, y, z), Quaternion.identity);
+                    else
+                    {
+                        rand = Random.Range(0, 10);
 
+                        if (rand <= 6 && rand >= 3 && perlinNoiseArray[x, z] > min + 2 && perlinNoiseArray[x, z] < max - 5)
+                        {
+
+                            Instantiate(tierraCesped, new Vector3(x, y, z), Quaternion.identity);
+
+                        }
+
+                        else Instantiate(tierra, new Vector3(x, y, z), Quaternion.identity);
+                    }
 
 
 
